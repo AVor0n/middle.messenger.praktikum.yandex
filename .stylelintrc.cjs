39b6a2e09b6512143e476bc29fe51df76e7680ea
Andrey Voronin <AVor0n@yandex.ru>
2023-08-08 01:10:33 +0300

    ci: add stylelint

---
+++ .stylelintrc.cjs
@@ -0,0 +1,20 @@
+const { propertyOrdering, selectorOrdering } = require('stylelint-semantic-groups');
+
+module.exports = {
+  extends: 'stylelint-config-recommended',
+  plugins: [
+    'stylelint-order',
+    'stylelint-use-logical-spec',
+    'stylelint-declaration-block-no-ignored-properties',
+  ],
+  rules: {
+    'color-hex-length': 'long',
+    'liberty/use-logical-spec': true,
+    'declaration-empty-line-before': null,
+    'plugin/declaration-block-no-ignored-properties': true,
+    'order/order': selectorOrdering,
+    'order/properties-order': propertyOrdering,
+    'no-descending-specificity': null,
+  },
+  defaultSeverity: 'error',
+};
