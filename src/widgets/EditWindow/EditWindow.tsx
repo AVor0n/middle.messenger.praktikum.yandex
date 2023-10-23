b5f0f09588e6a35a5bd8df67cc1cb0f866b815a4
Voronin Andrey <Andrey.Voronin@infotecs.ru>
2023-10-23 15:14:09 +0300

    feat(widgets): update EditWindow

--- src/widgets/EditWindow/EditWindow.tsx
+++ src/widgets/EditWindow/EditWindow.tsx
@@ -1,30 +1,34 @@
-import { Component, type Props } from '@shared';
-import { TextBox, type TextBoxProps } from '@uikit';
+import { Component } from '@shared';
+import type { PropsWithChildren } from '@shared';
 import './EditWindow.css';
 
-interface EditWindowProps extends Props {
-  fields: TextBoxProps[];
-}
+type EditWindowProps = PropsWithChildren<{
+  saveAvailable: boolean;
+  onSave: (e: Event) => void;
+  onClose: (e: Event) => void;
+}>;
 export class EditWindow extends Component<EditWindowProps> {
   constructor(props: EditWindowProps) {
     super({}, props);
   }
 
-  public render({ fields }: EditWindowProps) {
+  public render({ onClose, onSave, saveAvailable }: EditWindowProps) {
     return (
       <form className="edit-window">
         <div className="edit-window__content">
-          <div className="edit-window__fields">
-            {fields.map(fieldProps => (
-              <TextBox {...fieldProps} />
-            ))}
-          </div>
+          <div className="edit-window__fields">{this.props.children}</div>
 
           <div className="edit-window__btns">
-            <button id="editWindowCancel" className="btn btn--ghost btn--wide btn--l">
+            <button id="editWindowCancel" className="btn btn--ghost btn--wide btn--l" $click={onClose}>
               Отмена
             </button>
-            <button id="editWindowSave" className="btn btn--primary btn--wide btn--l">
+
+            <button
+              id="editWindowSave"
+              className="btn btn--primary btn--wide btn--l"
+              $click={onSave}
+              disabled={!saveAvailable}
+            >
               Сохранить
             </button>
           </div>
