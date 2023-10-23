145137fa08c7b20bf79d0837e0b0c880cc0fa7f7
Voronin Andrey <Andrey.Voronin@infotecs.ru>
2023-10-23 16:02:30 +0300

    refactor(utils): improve types

--- src/shared/lib/utils/common.ts
+++ src/shared/lib/utils/common.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-export const removeProxy = (record: Record<string, unknown>) => JSON.parse(JSON.stringify(record));
+export const removeProxy = <T extends Record<string, unknown>>(record: T): T => JSON.parse(JSON.stringify(record));
