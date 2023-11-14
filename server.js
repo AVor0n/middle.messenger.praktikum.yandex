275352b9ad0e7100ec1ff5e77d2dd91a7c4eabc8
Voronin Andrey <Andrey.Voronin@infotecs.ru>
2023-11-14 17:01:41 +0300

    fix(server): router don't work on express server

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import url from 'url';
+import path from 'path';
 import process from 'node:process';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.use(express.static('./dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`App run on port ${PORT}: http://localhost:${PORT}`);
