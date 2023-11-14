c6c2419e3ca2b6451016fdc3f5204ce544856be4
Voronin Andrey <Andrey.Voronin@infotecs.ru>
2023-11-14 17:20:47 +0300

    fix(chat): view chat members avatar

--- src/pages/Chat/components/EditChatMembersButton/components/UserPreview/UserPreview.tsx
+++ src/pages/Chat/components/EditChatMembersButton/components/UserPreview/UserPreview.tsx
@@ -3,6 +3,7 @@ import { clsx } from '@shared/utils';
 import * as styles from './UserPreview.module.css';
 import { type UserPreviewData } from '../../types';
 import { Avatar } from '@uikit';
+import { fileService } from 'services';
 
 type UserPreviewProps = PropsWithChildren<{
   userData: UserPreviewData;
@@ -16,10 +17,11 @@ export class UserPreview extends Component<UserPreviewProps> {
 
   public render() {
     const { userData } = this.props;
+    const avatarSrc = userData.avatar ? fileService.getLinkToFile(userData.avatar) : undefined;
 
     return (
       <div className={clsx(styles.container, this.props.className)}>
-        <Avatar containerCls={styles.avatar} />
+        <Avatar src={avatarSrc} containerCls={styles.avatar} />
         <h2 className={styles.name}>
           {userData.firstName} {userData.secondName}
         </h2>
