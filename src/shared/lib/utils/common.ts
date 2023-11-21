bd14323117a45c259d568ecd6194be932c92ed67
Voronin Andrey <Andrey.Voronin@infotecs.ru>
2023-11-21 19:05:20 +0300

    feat(utils): add sleep

--- src/shared/lib/utils/common.ts
+++ src/shared/lib/utils/common.ts
@@ -1,3 +1,10 @@
 export const removeProxy = <T extends Record<string, unknown>>(record: T): T => JSON.parse(JSON.stringify(record)) as T;
 
 export const clsx = (...classes: (string | undefined | null | boolean)[]) => classes.filter(Boolean).join(' ');
+
+export const sleep = (duration: number) =>
+  new Promise<void>(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, duration);
+  });
