da1af83f97b0bb84f7a1948b44c5c9a1b7ce54a4
Andrey Voronin <AVor0n@yandex.ru>
2023-11-22 02:50:52 +0300

    build(postcss): remove postcss-color-mod-function

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-nested'),
-    require('postcss-color-mod-function'),
     require('postcss-normalize'),
     require('autoprefixer'),
   ],
