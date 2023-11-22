882a07d3e3bdbb85cc78cdb29c29637a67381916
Voronin Andrey <Andrey.Voronin@infotecs.ru>
2023-11-22 19:38:15 +0300

    ci(husky): add run tests on pre-commit

--- .nano-staged.js
+++ .nano-staged.js
@@ -1,5 +1,5 @@
 export default {
-  'src/**/*.{ts,tsx}': () => ['tsc -p tsconfig.json  --noEmit'],
+  'src/**/*.{ts,tsx}': () => ['tsc -p tsconfig.json  --noEmit', 'mocha --bail'],
   '*.{ts,tsx}': ['eslint --fix', 'prettier --write'],
   '*.css': ['stylelint --fix', 'prettier --write'],
   '!(*.{ts,css})': ['prettier --write --ignore-unknown'],
