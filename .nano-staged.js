22884deafb9832db763d615afa16a1d824a5d7db
Voronin Andrey <Andrey.Voronin@infotecs.ru>
2023-11-22 19:37:31 +0300

    ci(husky): fix nano-staged config

--- .nano-staged.js
+++ .nano-staged.js
@@ -1,6 +1,6 @@
 export default {
-  'src/**/*.ts': () => ['tsc -p tsconfig.json  --noEmit'],
-  '*.ts': ['eslint --fix', 'prettier --write'],
+  'src/**/*.{ts,tsx}': () => ['tsc -p tsconfig.json  --noEmit'],
+  '*.{ts,tsx}': ['eslint --fix', 'prettier --write'],
   '*.css': ['stylelint --fix', 'prettier --write'],
   '!(*.{ts,css})': ['prettier --write --ignore-unknown'],
 };
